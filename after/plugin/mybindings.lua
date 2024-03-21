8c65120850562d710481907c5ea86a709bf68c15
halston <sellenth@oregonstate.edu>
2024-03-21 13:06:05 -0700

    mac

--- after/plugin/mybindings.lua
+++ after/plugin/mybindings.lua
@@ -6,6 +6,15 @@ vim.api.nvim_set_keymap('n', '<leader>co', ':Copilot enable<CR>', { noremap = tr
 vim.api.nvim_set_keymap('n', '<leader>cc', ':Copilot disable<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>w', '<c-w>', { noremap = true, silent = true })
 
+vim.keymap.set('n', '<leader>b', function() require('dap').toggle_breakpoint() end)
+--[[
+nnoremap <silent> <Leader>B <Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
+nnoremap <silent> <Leader>lp <Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
+nnoremap <silent> <Leader>dr <Cmd>lua require'dap'.repl.open()<CR>
+nnoremap <silent> <Leader>dl <Cmd>lua require'dap'.run_last()<CR>
+--]]
+
+
 local harpoon = require("harpoon")
 
 -- REQUIRED
